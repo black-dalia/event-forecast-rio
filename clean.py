f0a66faf82d118b5112362674c14564f96c12d25
Kavitha Appulingam <kav1323@hotmail.com>
2022-03-02 12:37:34 +0000

    clean data import

--- clean.py
+++ clean.py
@@ -5,81 +5,76 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import unidecode
 import fuzzymatcher
+from datetime import datetime
 
-# Data loading
 def get_data():
+
     data1 = pd.read_csv('raw_data/parte1.csv', sep=';', encoding = 'iso-8859-1')
     data2 = pd.read_csv('raw_data/parte2.csv', sep=';', encoding = 'iso-8859-1')
     data3 = pd.read_csv('raw_data/parte3.csv', sep=';', encoding = 'iso-8859-1')
     data4 = pd.read_csv('raw_data/parte4.csv', sep=';', encoding = 'iso-8859-1')
-    kept_columns = ['controle', 'titulo_do', 'total_rbft', 'cisp', 'data_fato', 'hora_fato', 'local', 'bairro_fato']
-    eng_columns = ['Crime_ID', 'Crime_sub_type', 'Crime_type', 'Police_station', 'Date', 'Time', 'Place_type', 'Neighborhood']
-    return data1, data2, data3, data4, kept_columns, eng_columns
+
+    return data1, data2, data3, data4
 
 # Import data with correct bairros and AR names
 def get_bairros_data():
-    bairros_all = pd.read_csv("raw_data/bairros_lista.csv", encoding='iso-8859-1')
+    bairros_all = pd.read_csv("raw_data/bairros_lista.csv", encoding="iso-8859-1")
     return bairros_all
-
-
 # Cleaning function
 def merge_clean(data1, data2, data3, data4):
-
     # Merging
     data = pd.concat([data1, data2, data3, data4]) # Merging all datasets
-
     # Focusing on Rio de Janeiro only
-    data = data[data['municipio_fato'] == 'Rio de Janeiro (Capital)'] # Filtering on Rio de Janeiro only
-
+    data = data[data["municipio_fato"] == "Rio de Janeiro (Capital)"] # Filtering on Rio de Janeiro only
     # Columns transforming
-    data = data[kept_columns] # Removing useless columns
-    data.columns = eng_columns  # Renaming columns in English
-
+    data = data[["controle", "titulo_do", "total_rbft", "cisp", "data_fato",\
+        "hora_fato", "local", "bairro_fato"]] # Removing useless columns
+    data.columns = ["Crime_ID", "Crime_sub_type", "Crime_type",\
+        "Police_station", "Date", "Time", "Place_type", "Neighborhood"]  # Renaming columns in English
     # Date and Time preprocessing
-    data = data[data['Time'] != '99'] # Removing invalid time format
-    data['Date_Time'] = pd.to_datetime(data['Date'] + ' '
-                                      + data['Time'], format='%Y-%m-%d %H:%M') # Passing to datetime format
-    data.drop(columns=['Date', 'Time'], inplace=True) # Removing time and date columns once the Date_Time is created
-    data = data[data['Date_Time'] > '2008-01-31'] # Removing irrelevant date samples
-
+    data = data[data["Time"] != "99"] # Removing invalid time format
+    data = data.dropna(subset = ['Date']) # Removing missing values (for date)
+    data["Test_Date_Time"] = data["Date"] + " " + data["Time"] # Combining Date and Time
+    data["Date_Time"] = pd.to_datetime(data["Test_Date_Time"]) # Passing to DateTime format
+    data.drop(columns=["Date", "Time"], inplace=True) # Removing time and date columns once the Date_Time is created
+    data = data[data["Date_Time"] > "2008-12-31"] # Removing irrelevant date samples
     # Missing values, duplicates & text standardizing
     data = data.drop_duplicates(subset="Crime_ID") # Removing duplicates
-    data['Neighborhood'] = data['Neighborhood'].map(lambda x: unidecode.unidecode(x)) # Removing accents
-    data = data[data['Neighborhood'] != 'sem informacao'] # Removing missing values for neighborhood
-
+    data["Neighborhood"] = data["Neighborhood"].map(lambda x: unidecode.unidecode(x)) # Removing accents
+    data = data[data["Neighborhood"] != "sem informacao"] # Removing missing values for neighborhood
     return data
-
 # Bairra detail
 def barra_replace(row):
     if row == "Barra":
         row= "Barra da Tijuca"
     return row
-
 # Bairros/AR matching
 def get_AR(data, ar_data): #data_AR should be the full table with bairros and AR
-
-    data['Neighborhood'] = data['Neighborhood'].map(barra_replace) # Setting the good "Barra da Tijuca" name
-
+    data["Neighborhood"] = data["Neighborhood"].map(barra_replace) # Setting the good "Barra da Tijuca" name
     # Bairros matching
-    bairros = pd.DataFrame(ar_data, columns=["Bairro"]) # Creating a table with only the bairros
-    # bairros.set_index("Bairro")
-    data = fuzzymatcher.fuzzy_left_join(data, bairros, left_on="Neighborhood", right_on="Bairro") # Replacing non-standardized bairros names with standardized ones
-    data = data.drop(columns=['best_match_score', '__id_left', '__id_right', 'Neighborhood']) # Removing useless columns
-    data.rename(columns={'Bairro': 'Neighborhood'}, inplace=True)
-    data = pd.merge(data,ar_data,left_on='Neighborhood', right_on='Bairro',how='left').drop(columns=["Regiao","IDS","Bairro",'N¼']) # Aggreagting the right AR names to our new bairros
-    data.rename(columns={'R.A': 'RA'}, inplace=True)
-    data = data.dropna(subset=['RA']) # Removing lines with no bairros/AR information
-
+    bairros = pd.DataFrame(ar_data, columns=["Bairro"])
+    # Creating a table with only the bairros
+    bairros.set_index("Bairro")
+    data = fuzzymatcher.fuzzy_left_join(data, bairros, left_on="Neighborhood",\
+        right_on="Bairro") # Replacing non-standardized bairros names with standardized ones
+    data = data.drop(columns=["best_match_score",\
+        "__id_left", "__id_right", "Neighborhood"]) # Removing useless columns
+    data.rename(columns={"Bairro": "Neighborhood"}, inplace=True)
+    data = pd.merge(data,ar_data,left_on="Neighborhood",\
+        right_on="Bairro",how="left").drop(columns=["Regiao","IDS","Bairro","N¼"]) # Aggreagting the right AR names to our new bairros
+    data.rename(columns={"R.A": "AR"}, inplace=True)
+    data = data.dropna(subset=["AR"]) # Removing lines with no bairros/AR information
     return data
-
 # Cleaning - all
 def clean_all(data1, data2, data3, data4, ar_data):
     data = merge_clean(data1, data2, data3, data4)
     data = get_AR(data, ar_data)
-
     return data
 
+
 if __name__ == "__main__":
-    data1, data2, data3, data4, kept_columns, eng_columns = get_data()
+    print("start time  =", datetime.now())
+    data1, data2, data3, data4 = get_data()
     data = clean_all(data1, data2, data3, data4, get_bairros_data())
     print(data.head(5))
+    print("end time  =", datetime.now())
---
+++ preproc.py
@@ -0,0 +1,94 @@
+# Import section
+from datetime import datetime
+from webbrowser import get
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from clean import clean_all, get_data, get_bairros_data
+
+def get_format(data):
+    '''Function taking a clean dataset and returning 2 ARIMA-friendly df:
+    1. total nb_crime / day and AR
+    2. nb_crime per 1000 inhab / day and AR'''
+
+    data["Date"] = data["Date_Time"].dt.date
+    preprocessed_data = data.groupby(["AR", "Date"]).count()[["Crime_ID"]]
+    preprocessed_data.rename(columns={"Crime_ID":"nb_crimes"}, inplace=True)
+
+    ## add a column with yearly population per AR
+    pop_clean = clean_pop_data()
+    preprocessed_data["year_temp"] = preprocessed_data.index.map(lambda x: x[1].year)
+    input_merge = preprocessed_data.reset_index()
+    data_merge = pd.merge(left=input_merge, right=pop_clean, left_on=["AR","year_temp"],\
+        right_on=["administrative_regions", "Ano"])
+    data_merge.drop(columns=["administrative_regions","Ano"], inplace = True)
+    preprocessed_data.drop(columns=["year_temp"],inplace = True)
+    ## compute nb crimes / 1000 inhabitants
+    data_merge["nb_crimes_1000"]=data_merge.nb_crimes / data_merge.Populacao*1000
+    data_merge.drop(columns=["nb_crimes","Populacao","year_temp"], inplace=True)
+    data_merge.rename(columns={'nb_crimes_1000': 'nb_crimes'},inplace=True)
+    data_merge.set_index(["AR","Date"],inplace=True)
+    ##
+    preprocessed_data = preprocessed_data.unstack(level=0)
+    preprocessed_data = preprocessed_data.replace(np.nan, 0).astype(int)
+    preprocessed_data_1000 = data_merge.unstack(level=0)
+    preprocessed_data_1000 = preprocessed_data_1000.replace(np.nan, 0)
+    return preprocessed_data, preprocessed_data_1000
+
+def get_popfile():
+    '''import population file'''
+    return pd.read_csv("raw_data/population_Rio.csv", sep=",")
+
+def clean_pop_data():
+    data= get_popfile()
+    data = data.drop(columns=["DensidadeBruta", "DensidadeLiquida", "TaxaGeometrica"])
+
+    # Get rid of Roman numbers in front of name of administrative regions
+    splitted_regions = data['RegiaoAdministrativa'].str.split().str[1:]
+    cleaned_regions = splitted_regions.str.join(" ")
+    data["administrative_regions"] = cleaned_regions
+    data = data.drop(columns=["RegiaoAdministrativa"])
+
+    # Dictionary of population data per region for 2000-2020 in 5-years steps
+    regions_dict = {}
+    for region in data["administrative_regions"].unique():
+        regions_dict[region] = data[data["administrative_regions"]==region]
+
+    # Dataframe with years for 2000-2020 in 1-year steps
+    year_df = pd.DataFrame(pd.period_range(min(data.Ano), max(data.Ano), freq="Y"), columns=["Ano"])
+    year_df = year_df[["Ano"]].astype("str").astype("int64")
+
+    # Extend time series to annual time series and interpolate lineraly the missing population data
+    middle_dict = {}
+    for k, v in regions_dict.items():
+        middle_dict[k] = year_df.merge(v, how="left", on="Ano")
+        middle_dict[k]["administrative_regions"].fillna(value=k, inplace=True)
+        middle_dict[k]["Populacao"].interpolate(method='linear', inplace=True)
+
+
+    # Create a new dataframe with cleand and extended population data
+    empty = pd.DataFrame(columns=["Ano", "Populacao", "administrative_regions"])
+    df = pd.concat(middle_dict).reset_index().drop(columns=["level_0", "level_1"])
+
+    return df
+
+def extract_ts(df, AR):
+    '''Extract the time series from selected df and for each AR'''
+    df1 = df.reset_index()
+    df2 = df1[[(     'Date',                   ''), ('nb_crimes',AR)]]
+    df2.columns = df2.columns.droplevel()
+    df2.columns=["ds", "y"]
+    return df2
+
+if __name__ == "__main__":
+    print("start time  =", datetime.now())
+    data1, data2, data3, data4 = get_data()
+    data = clean_all(data1, data2, data3, data4, get_bairros_data())
+    preprocessed_data, preprocessed_data_1000 = get_format(data)
+    pop_clean = clean_pop_data()
+    print(pop_clean.head())
+    print(preprocessed_data.tail(10))
+    print(preprocessed_data_1000.tail(10))
+
+    print("end time  =", datetime.now())

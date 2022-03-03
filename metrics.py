9037c467d58f52422bdc03f3d51cb71dc296839a
Ann-Christin Klein ann-christin.klein@gmx.net <ann-christin.klein@gmx.net>
2022-03-03 12:14:38 +0000

    Update in metrics.py

--- metrics.py
+++ metrics.py
@@ -1,5 +1,34 @@
 import numpy as np
+import pandas as pd
 
+# Baseline model predicts daily values of crime per 1000 inhabitants per region for 2019 based on the
+# annual mean of crime per 1000 inhabitants per region for 2018.
+# Prediction horizon is one month, we are predicting daily values for January 2021 (31 days).
+
+def get_baseline_data(path):
+    # Preprocessing of baseline data
+    df = pd.read_csv(path)
+    df.columns = df.iloc[0, :]
+    df = df.iloc[2:, :]
+    df["AR"] = pd.to_datetime(df.AR)
+    region_columns = list(df.columns)[1:]
+    for column in region_columns:
+        df[column] = pd.to_numeric(df[column])
+    return df
+
+# From the function below you can select the column of the required region to obtain y_pred_baseline.
+# This can then be passed to the forecast_accuracy function as "forecast".
+
+def get_baseline_actual(df):
+    baseline_month_actual = df[(df['AR'] >= "2019-01-01") & (df['AR'] < "2019-02-01")].reset_index().drop(columns="index")
+    return baseline_month_actual
+
+def get_baseline_predictions(df):
+    baseline_data = df[(df['AR'] >= "2018-01-01") & (df['AR'] < "2019-01-01")]
+    baseline_data = baseline_data.reset_index().drop(columns="index")
+    baseline_mean = baseline_data.mean().to_frame().T
+    baseline_month_forecast = pd.concat([baseline_mean]*31).reset_index().drop(columns="index")
+    return baseline_month_forecast
 
 def forecast_accuracy(forecast, actual):
     forecast = forecast.to_numpy()
@@ -19,3 +48,17 @@ def forecast_accuracy(forecast, actual):
     return({'mape':mape, 'me':me, 'mae': mae,
             'mpe': mpe, 'rmse':rmse, #'acf1':acf1,
             'corr':corr, 'minmax':minmax})
+
+# Note: Make sure to pass the region in quotation marks, such as "Mare" or "Anchieta"
+def get_baseline_metrics(region, df):
+    baseline_month_actual_region = get_baseline_actual(df).region
+    baseline_month_forecast_region = get_baseline_predictions(df).region
+
+    baseline_metrics = forecast_accuracy(baseline_month_forecast_region, baseline_month_actual_region)
+    return baseline_metrics
+
+# How to run this file entirely:
+    # df = get_baseline_data(path)
+    # baseline_month_forecast = get_baseline_predictions(df)
+    # baseline_month_actual = get_baseline_actual(df)
+    # baseline_metrics = get_baseline_metrics(region, df)
